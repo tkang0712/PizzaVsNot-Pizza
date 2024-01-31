8f275c143b87884eead1e9b153f3711304baabac
TheoKang1 <TheoKang1@gmail.com>
2024-01-30 19:08:51 -0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from fastai.vision.all import *
+st.write("Pizza Vs Not Pizza Classifier")
+st.text("Builtport stream by Theo Kang")
+
+def label_func(f): return f[0].isupper()
+
+
+model = load_learner('my_model (1).pkl')
+
+def predict(image):
+    img = PILImage.create(image)
+    pred_class, pred_idx, outputs = model.predict(img)
+    likelihood_is_pizza = outputs[1].item()
+    if likelihood_is_pizza == 1:
+        return "Pizza"
+    elif likelihood_is_not_pizza < 0.01:
+        return "Not Pizza"
+    else:
+        return "Not sure... try another picture!"
+st.title("Pizza vs. Not-Pizza Classifier")
+st.write("Upload an image, and I'll tell you whether it's pizza or not-pizza.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+    if st.button("Predict"):
+        prediction = predict(uploaded_file)
+        st.write(prediction)
+
+st.text("Built with Streamlit and Fastai")
